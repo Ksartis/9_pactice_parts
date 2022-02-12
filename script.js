3b234e8d4f0c93a11006aeb5d07e08153a6d0cd0
Viktor <example@gmail.com>
2022-02-12 14:20:58 +0300

    Не удалось решить задачу

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
+if (numberOfFilms < 10) {
+    alert('Просмотрено довольно мало фильмов');
+} else if (numberOfFilms >= 10 && numberOfFilms < 30 ) {
+    alert('Вы классический зритель');
+}  else if (numberOfFilms >=30) { 
+    alert('Вы киноман');
+} else {
+    alert('Произошла ошибка');
+}
+
+
 const personalMovieDB = {
     count: numberOfFilms,
     movies:{},
@@ -8,12 +19,28 @@ const personalMovieDB = {
     privat: false
 };
 
-const titleMovie1 = prompt('Один из просмотренных фильмов',''),
-    scoresMovie1 = prompt('На сколько оцениваете его?',''),
-    titleMovie2 = prompt('Один из просмотренных фильмов',''),
-    scoresMovie2 = prompt('На сколько оцениваете его?','');
+//  let titleMovie = prompt('Один из просмотренных фильмов','');
+// let scoresMovie = prompt('На сколько оцениваете его?','')
+  
 
-personalMovieDB.movies[titleMovie1] = scoresMovie1;
-personalMovieDB.movies[titleMovie2] = scoresMovie2;
+// for(let i = 1; i <= 33;i++){
+//       personalMovieDB.movies[titleMovie] = scoresMovie;
+// } 
+
+
+
+for(let i = 1; i <= 2;i++){
+    let titleMovie = prompt('Один из просмотренных фильмов','');
+    
+    
+    if (titleMovie.length === 0 || titleMovie.length >= 50 || titleMovie === ' ' || titleMovie === null ){
+        prompt('вы меня не поняли?','');
+    } else {
+        let scoresMovie = prompt('На сколько оцениваете его?','');  
+    }
+
+
+    personalMovieDB.movies[titleMovie] = scoresMovie;
+} 
 
 console.log(personalMovieDB);
